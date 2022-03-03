5ca84e9f1e1f59421ce12cd2063a57bd5a5e44d3
tjgarcia-mchp <75396415+tjgarcia-mchp@users.noreply.github.com>
2022-03-03 16:20:04 -0600

    add flag for switch app type; bug fix; refactor
    Added flag in app_config to switch between data streamer and level meter.
    
    Modified project properties to add delay after reset - this seems to fix an
    occasional issue where the board would not start up properly right after
    programming.
    
    Updated README with more instruction.

--- README.md
+++ README.md
@@ -1,20 +1,45 @@
+# Overview
 This branch is a template for implementing microphone audio applications on the
 SAME54 Curiosity Ultra + wm8904 codec. Audio input is formatted as 16-bit PCM
 (signed Q1.15) at 16kHz.
 
-See `firmware/src/app_config.h` to configure buffer sizes.
+# Firmware
+The firmware can be configured for two different build types: (1) a simple level
+meter and (2) a UART data streamer; this selection can be made by modifying the
+`BUILD_APPLICATION_TYPE` value in `app_config.h`
 
-By default, application will stream data over UART at a baud rate of 921600:
+### APPLICATION_TYPE_LEVEL_METER
+This application just outputs the measured signal level over UART at a baud rate
+of 921600.
 
-* Data is sent in frames containing `AUDIO_BLOCK_NUM_SAMPLES` samples
+### APPLICATION_TYPE_DATA_STREAMER
+This application will stream data over UART:
+
+* Transmits at a baud rate of 921600
+* Data is sent in frames containing `AUDIO_BLOCK_NUM_SAMPLES` samples (see `app_config.h`)
 * Frames are separated by a header byte (0xa5) and footer byte (0x5a)
+* Frame size can be configured by modifying `AUDIO_BLOCK_SIZE_MS` in `app_config.h`.
 
+The `readserial.py` script described below can be used to capture the audio data.
+
+# Capturing Streaming Audio
 The `readserial.py` script provides an example of how to parse out this data and
 save it to a .wav file:
 
 * Requires python 3
-* Requires a few python packages; to install, run: `pip install -r requirements.txt`
+* Requires a few python packages: to install, run:
+  > `pip install -r requirements.txt`
 * Script can be run interactively via IPython or Jupyter notebook, or directly
   from the terminal using the following call format:
-  * `readserial.py <com-port> <baud-rate> <burst-size> <num-samples>`
-  * e.g.: `readserial.py COM7 921600 512 16000`
+  > `readserial.py <com-port> <baud-rate> <burst-size> <num-samples>`
+
+  For example:
+  > `readserial.py COM7 921600 512 16000`
+* Running the script will stream audio from the device and save it to a wav file
+named `readserial.wav`.
+
+# Known Issues
+
+* Occasionally I've found that the board needs to be reset after programming for
+  the program to work. Adding delay after reset seemed to fix this (*Project
+  Properties -> EDBG -> Diagnostics -> Additional delay after reset*).
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -53,14 +53,16 @@ size_t __attribute__(( unused )) UART_Write(uint8_t *ptr, const size_t nbytes) {
 // Section: Main Entry Point
 // *****************************************************************************
 // *****************************************************************************
-
 int main ( void )
 {
+#if BUILD_APPLICATION_TYPE == APPLICATION_TYPE_LEVEL_METER    
+    float dbmeter = 0;
+    float alpha = 0.96; // recursive smoothing constant
+#endif
+    
     /* Initialize all modules */
     SYS_Initialize ( NULL );
     
-//    float dbmeter = 0;
-//    float alpha = 0.9;
     while ( appData.state != APP_STATE_FATAL )
     {
         /* Maintain state machines of all polled MPLAB Harmony modules. */
@@ -70,35 +72,43 @@ int main ( void )
         ringbuffer_size_t rdcnt;
         snsr_data_t const *ptr = ringbuffer_get_read_buffer(&micBuffer, &rdcnt);
 
-//        // sqrt(1/n * sum(x^2))
-//        float db = 0;
-//        for (size_t i=0; i < AUDIO_BLOCK_NUM_SAMPLES; i++) {
-//            float x = (float) ptr[i] / 32768;
-//            db += x*x;
-//        }
-
         while (rdcnt--) {
+#if BUILD_APPLICATION_TYPE == APPLICATION_TYPE_DATA_STREAMER
             uint8_t headerbyte = 0xA5U;
             UART_Write(&headerbyte, 1);
             UART_Write((uint8_t *) ptr, micBuffer.itemsize);
             headerbyte = ~headerbyte;
             UART_Write(&headerbyte, 1);
+#elif BUILD_APPLICATION_TYPE == APPLICATION_TYPE_LEVEL_METER
+            // Compute RMS level: sqrt(1/n * sum(x^2))
+            float db = 0;
+            
+            // sum(x^2)
+            for (size_t i=0; i < AUDIO_BLOCK_NUM_SAMPLES; i++) {
+                float x = (float) ptr[i] / 32768; // Convert 16-bit PCM to [-1,1)
+                db += x*x;
+            }
+            // 1/n
+            db /= AUDIO_BLOCK_NUM_SAMPLES;
+            
+            // convert to decibel
+            db = 10 * log10(db + 1e-9); // Floor level at -90dB
+            dbmeter = alpha*dbmeter + (1-alpha)*db;
+            
+            // Convert to 1-10 integer volume level
+            int vol = (int) ((90 + db) / 9);
 
-            ringbuffer_advance_read_index(&micBuffer, 1);
+            // e.g.: -60 dB | [===       ]
+            printf("%3.0fdB | [", dbmeter);
+            for (int i=0; i < vol; i++)
+                printf("=");
+            for (int i=0; i < 10-vol; i++)
+                printf(" ");
+            printf("]\r");
+#endif
             ptr += AUDIO_BLOCK_NUM_SAMPLES;
+            ringbuffer_advance_read_index(&micBuffer, 1);
         }
-
-//        db /= AUDIO_BLOCK_NUM_SAMPLES;
-//        db = 10 * log10(db + 1e-9);
-//        dbmeter = alpha*dbmeter + (1-alpha)*db;
-//        int vol = (int) ((90 + db) / 9);
-//
-//        printf("%6.2fdB | [", dbmeter);
-//        for (int i=0; i < vol; i++)
-//            printf("=");
-//        for (int i=0; i < 10-vol; i++)
-//            printf(" ");
-//        printf("]\r");
     }
 
     /* Execution should not come here during normal operation */
